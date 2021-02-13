80f3003212724ebdcace82ad457610a7b9676c75
francischempa <francis.chempa@icloud.com>
2021-02-13 20:22:46 +0000

    name of project is src"

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/dbc_sa_gui'));
+app.use(express.static('./dist/src'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/dbc_sa_gui/'}),
+    res.sendFile('index.html', {root: 'dist/src/'}),
 );
 
 // Start the app by listening on the default Heroku port
